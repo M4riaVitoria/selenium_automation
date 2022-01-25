6c75190f5f1234b851335ee937afad48a9f8c807
M4ria <79952793+M4riaVitoria@users.noreply.github.com>
2022-01-25 14:52:10 -0300

    Add files via upload

---
+++ testemb.py
@@ -0,0 +1,23 @@
+import pyautogui
+import selenium
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+navegador = webdriver.Chrome()
+
+navegador.get("https://sistemas.grupocoutinho.com/")
+
+for i in range(13):
+    pyautogui.press("tab")
+
+pyautogui.write("")
+pyautogui.press("tab")
+pyautogui.write("")
+pyautogui.press("enter")
+pyautogui.press("tab")
+pyautogui.press("enter")
+pyautogui.press("tab")
+pyautogui.press("enter")
+
+pyautogui.press("tab")
+pyautogui.press("enterrrr")
